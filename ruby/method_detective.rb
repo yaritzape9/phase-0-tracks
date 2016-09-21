41cf4ae0bba910316d8b158b7794dc90aa697b1d
Yaritza Perez <yaritzape9@gmail.com>
2016-09-20 20:10:32 -0700

    added methods to the file

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -4,36 +4,38 @@
 # When there's more than one suspect who could have
 # committed the crime, add additional calls to prove it.
 
-# "iNvEsTiGaTiOn".<???>
+ puts "iNvEsTiGaTiOn".swapcase
 # => “InVeStIgAtIoN”
 
-# "zom".<???>
+ puts "zom".insert(1,'o')
 # => “zoom”
 
-# "enhance".<???>
+  puts"enhance".center(20)
 # => "    enhance    "
 
-# "Stop! You’re under arrest!".<???>
+ puts "Stop! You’re under arrest!".upcase
 # => "STOP! YOU’RE UNDER ARREST!"
 
-# "the usual".<???>
+ puts "the usual".insert(9,' suspects')
 #=> "the usual suspects"
 
-# " suspects".<???>
+ puts " suspects".prepend('the usual')
 # => "the usual suspects"
 
-# "The case of the disappearing last letter".<???>
+ puts "The case of the disappearing last letter".chomp("r")
+ puts "The case of the disappearing last letter".chop
 # => "The case of the disappearing last lette"
 
-# "The mystery of the missing first letter".<???>
+ puts "The mystery of the missing first letter".delete("T")
 # => "he mystery of the missing first letter"
 
-# "Elementary,    my   dear        Watson!".<???>
+ puts "Elementary,    my   dear        Watson!".squeeze
 # => "Elementary, my dear Watson!"
 
-# "z".<???>
+ puts "z".bytes
 # => 122 
 # (What is the significance of the number 122 in relation to the character z?)
+#the amount of bytes 122 has
 
-# "How many times does the letter 'a' appear in this string?".<???>
+ puts "How many times does the letter 'a' appear in this string?".count("a")
 # => 4
