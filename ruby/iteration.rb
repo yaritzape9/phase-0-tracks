c77d253dc9384f1b3831858a5e441afa4ffc09bf
Yaritza Perez <yaritzape9@gmail.com>
2016-09-24 18:22:44 -0600

    added simple array and hashes

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -22,31 +22,110 @@ dog_owners = {
 dog_owners.each do |name, breed|
 	puts "#{name} owns a #{breed}"
 end
-	
-random_names = ["Kobe", "Alaska", "Elijah", "Klaus", "Rebecca", "Felicity"]
+#delete method	
 
-random_names.each do |names| 
-	index = 0 
-		while index < random_names.length
-			if (random_names[index] == random_names.length)
-			random_names.delete(0)
-end
+# random_nums = [1,5,7,3,8]
 
-p random_names
+#1 
 
+#arrays 
 
-age = { 
-	Kobe: "6", 
-	Alaska: "17", 
-	Elijah: "27",
-	Klaus: "24",
-	Rebecca: "19",
-	Felicity: "22",
-}
+# random_nums.delete_if do |numbers| 
+# 	if numbers < 5 
+# 		true
+# 	else 
+# 		false
+# 	end
+# end
 
+#hashes 
 
+# random = { 
+# 	"one": 1,
+# 	"two": 2,
+# 	"three": 3,
+# 	"four": 4,
+# 	"five": 5 
+# } 
 
+# random.delete_if do |word, number| 
+# 	number < 2
+# end 
 
+#2
 
+#arrays 
 
+# random_nums.keep_if do |numbers| 
+# 	if numbers < 5 
+# 		true
+# 	else 
+# 		false
+# 	end
+# end 
 
+#hashes 
+
+# random = { 
+# 	"one": 1, 	
+# 	"two": 2,
+# 	"three": 3,
+# 	"four": 4,
+# 	"five": 5
+# }
+
+# random.keep_if do |word, numbers|
+# 	numbers < 3 
+# end
+
+#3 
+
+#arrays 
+
+# random_nums.select do |numbers|
+# 	if numbers < 5 
+# 		puts numbers
+# 	end
+# end 
+
+#hashes 
+
+# random = { 
+# 	"one": 1,
+# 	"two": 2, 
+# 	"three": 3,
+# 	"four": 4, 
+# 	"five": 5
+# } 
+
+# random.select do |word, numbers| 
+# 	numbers < 3
+# end 
+
+#4 
+
+#arrays 
+
+# random_nums.reject! do |numbers|
+# 	if numbers < 5 
+# 		true
+# 	else 
+# 		false
+# 	end
+# end 
+
+# puts random_nums
+
+#hashes 
+
+random = { 
+	"one": 1,
+	"two": 2,
+	"three": 3,
+	"four": 4,
+	"five": 5 
+} 
+
+random.reject! do |words, numbers| 
+	numbers < 4 
+end 
