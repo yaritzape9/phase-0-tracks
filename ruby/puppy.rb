f9e1cebac04646b699e73ca7931b7bf46cfe9b4a
Yaritza Perez <yaritzape9@gmail.com>
2016-09-30 00:14:11 -0700

    added puppy class psuedocode

---
+++ ruby/puppy.rb
@@ -0,0 +1,16 @@
+#Class Puppy
+
+	# CHARACTERISTICS ---------------
+	# Friendly: yes
+	# Eye count: 8
+	# Color: varies
+	# Size: varies
+	# Barks: yes
+
+	# BEHAVIOR -----------------------
+	# Jump
+	# lick
+	# bark
+	# cuddly
+	# adventurous
+	# fit
