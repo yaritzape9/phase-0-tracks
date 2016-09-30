5ce30175939c932728d6b0ad52a9bee17d6e368b
Yaritza Perez <yaritzape9@gmail.com>
2016-09-30 00:07:11 -0700

    added methods to puppy

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -4,6 +4,67 @@ class Puppy
     puts "I brought back the #{toy}!"
     toy
   end
+  
+  def speak(bark)
+    bark.times do
+      puts "woof"
+    end
+  end
+
+  def roll_over
+    puts "*Rolls over*"
+  end
+
+  def dog_years(years)
+    p years * 4
+  end
+
+  def sit
+    puts "*sits*"
+  end
+
+  def initialize
+    puts 'Initializing new puppy instance ...'
+  end
+end
+# Puppy.new.sit
+# Puppy.new.dog_years(21)
+# Puppy.new.roll_over
+# Puppy.new.speak(3)
+
+
+class Gymnast
+  def flip
+    puts "*does flip*"
+  end
+
+  def jump
+    puts "*does jump*"
+  end
+
+  def pommelhorse
+    puts "*does rotation*"
+  end
+end
+
+fifty_shades_of_instance = Array.new
+
+Gymnast.new.flip
+Gymnast.new.jump
+Gymnast.new.pommelhorse
+
+index = 0
+while index < 50
+fifty_shades_of_instance[index] = Gymnast.new
+p fifty_shades_of_instance
+index += 1
+end
+
+fifty_shades_of_instance.each do |grey|
+grey.jump
+grey.flip
+grey.pommelhorse
+end
 
 end
 
