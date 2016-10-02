0e21b456bb8257033b1f84c13dfdf959fce8ebc9
Yaritza Perez <yaritzape9@gmail.com>
2016-10-01 22:25:22 -0700

    added my reflection

--- ruby/gps2_2.rb
+++ ruby/gps2_2.rb
@@ -40,3 +40,20 @@ add_item(grocery_list, "ice cream", 4)
 remove_item(grocery_list, "lemonade")
 quantity_update(grocery_list, "ice cream", 1)
 print_method(grocery_list)
+
+
+# # What did you learn about pseudocode from working on this challenge?
+# 	Its very helpful when it comes to working with a pair because you both 
+# 	can be on the same page on what you want to make
+# # What are the tradeoffs of using arrays and hashes for this challenge?
+# 	# Its helpful because you can store much more info ina cleaner way.
+# # What does a method return?
+# 	A value that you made that method do.
+# # What kind of things can you pass into methods as arguments?
+	# arguments are passes so you can pass in things that statisfy those
+# 	# arguments.
+# # How can you pass information between methods?
+# 	# Having gloabal variables is a way to pass info between methods.
+# # What concepts were solidified in this challenge, and what concepts are still confusing?
+# 	Using more methods and having them work with one another. Like 
+# 	calling a method inside another method.
