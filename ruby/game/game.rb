60201bbb101b8fb196045de150a1be97b69e1848
Yaritza Perez <yaritzape9@gmail.com>
2016-10-02 23:25:25 -0700

    added a word input

--- ruby/game/game.rb
+++ ruby/game/game.rb
@@ -1,11 +1,11 @@
 
 # class guessinggame
-# 	puts the welcome message to the user
-# 	allows user input to get the word the other user is going to guess
-#	store that word into a variable 
-#	print a statement that says please guess a letter
-# 	write a loop that allows a certain amount of guesses (that being the length of the word)..also going to need a counter
-# 	the user will get feedback of there guesses in the example of ---- which will fill in the gaps if the answer a correct letter.
+#   puts the welcome message to the user
+#   allows user input to get the word the other user is going to guess
+# store that word into a variable 
+# print a statement that says please guess a letter
+#   write a loop that allows a certain amount of guesses (that being the length of the word)..also going to need a counter
+#   the user will get feedback of there guesses in the example of ---- which will fill in the gaps if the answer a correct letter.
 
 class GuessingGame
   attr_reader :guess_count
@@ -13,32 +13,36 @@ class GuessingGame
   attr_reader :secret_word
   
   def initialize
-  	@guess_count = 0
+    @guess_count = 0
     @is_over = false
     @secret_word = secret_word
    end
    
-   def check_word(index)
-  	@guess_count += 1
-	  	# if @s[index] == "ball"
-	  	# 	@is_over = true
-	  	# else
-	  	# 	false
-  		#end
-  	end
+   def check_word(secret_word)
+    @guess_count += 1
+      # if @s[index] == "ball"
+      #   @is_over = true
+      # else
+      #   false
+      #end
+    end
 end
 
  
 # user interface
 
 puts "Welcome to the Guess the word Game!"
-game = GuessingGame.new
+
+puts "What word would you like your oppnent to guess??"
+ secret_word = gets.chomp.downcase
+ game = GuessingGame.new(secret_word)
+
 
 while !game.is_over
   puts "Please guess a letter"
   guess = gets.chomp.to_i
   if !game.check_word(guess - 1)
-  	puts "Nope! Try again."
+    puts "Nope! Try again."
   end
 end
 
