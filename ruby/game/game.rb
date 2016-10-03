178b99e9738f6084a6652dcdf8c38820480490d2
Yaritza Perez <yaritzape9@gmail.com>
2016-10-03 05:04:20 -0700

    added better pseudo code

--- ruby/game/game.rb
+++ ruby/game/game.rb
@@ -1,12 +1,17 @@
 
 # class guessinggame
-#   puts the welcome message to the user
-#   allows user input to get the word the other user is going to guess
-# store that word into a variable 
-# print a statement that says please guess a letter
-#   write a loop that allows a certain amount of guesses (that being the length of the word)..also going to need a counter
-#   the user will get feedback of there guesses in the example of ---- which will fill in the gaps if the answer a correct letter.
-
+  #UI
+    #   puts the welcome message to the user
+    #   allows user input to get the word the other user is going to guess
+    # store that word into a variable 
+    # print a statement that says please guess a letter
+    #   write a loop that allows a certain amount of guesses (that being the length of the word)
+  #Methods
+    #initialize the variables im going to use
+    #have a guess method that checks if the letter you guessed is correct
+    #Have a hidden word method that hides the word 
+      #   the user will get feedback of there guesses in the example of ---- which will fill in the gaps if the answer a correct letter.
+    #have a determain game method which will tell you if you win or lose and also limits the amount of guesses you have
 class GuessingGame
   attr_reader :guess_count
   attr_reader :is_over
