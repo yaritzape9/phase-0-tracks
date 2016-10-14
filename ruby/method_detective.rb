ec9b0190f8e0eeea0a36535384a9d91c52eebeb7
yaritza perez <yaritzape9@gmail.com>
2016-10-14 05:38:09 -0700

    Deleted code and redoing

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -4,38 +4,38 @@
 # When there's more than one suspect who could have
 # committed the crime, add additional calls to prove it.
 
- puts "iNvEsTiGaTiOn".swapcase
-# => “InVeStIgAtIoN”
-
- puts "zom".insert(1,'o')
+puts "iNvEsTiGaTiOn".<??>
+	# => “InVeStIgAtIoN”
+	
+puts "zom".<??>
 # => “zoom”
 
-  puts"enhance".center(20)
+puts "enhance".<??>
 # => "    enhance    "
 
- puts "Stop! You’re under arrest!".upcase
+puts "Stop! You’re under arrest!".<??>
 # => "STOP! YOU’RE UNDER ARREST!"
 
- puts "the usual".insert(9,' suspects')
+puts "the usual".<??>
 #=> "the usual suspects"
 
- puts " suspects".prepend('the usual')
+puts " suspects".<??>
 # => "the usual suspects"
 
- puts "The case of the disappearing last letter".chomp("r")
- puts "The case of the disappearing last letter".chop
+puts "The case of the disappearing last letter".<??>
+puts "The case of the disappearing last letter".<??>
 # => "The case of the disappearing last lette"
 
- puts "The mystery of the missing first letter".delete("T")
+puts "The mystery of the missing first letter".<??>
 # => "he mystery of the missing first letter"
 
- puts "Elementary,    my   dear        Watson!".squeeze
+puts "Elementary,    my   dear        Watson!".<??>
 # => "Elementary, my dear Watson!"
 
- puts "z".bytes
+puts "z".<??>
 # => 122 
 # (What is the significance of the number 122 in relation to the character z?)
-#the amount of bytes 122 has
+# 122 in relation with the character z, is that 122 is the number of bytes within the string z.
 
- puts "How many times does the letter 'a' appear in this string?".count("a")
+puts "How many times does the letter 'a' appear in this string?".<??>
 # => 4
