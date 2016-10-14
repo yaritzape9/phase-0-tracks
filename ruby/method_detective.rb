a0f27a0002f37262c2644f4352573216c56bd346
yaritza perez <yaritzape9@gmail.com>
2016-10-14 06:20:21 -0700

    added all the methods for string class, that I could find

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -4,38 +4,68 @@
 # When there's more than one suspect who could have
 # committed the crime, add additional calls to prove it.
 
-puts "iNvEsTiGaTiOn".<??>
-	# => “InVeStIgAtIoN”
-	
-puts "zom".<??>
-# => “zoom”
+ p "iNvEsTiGaTiOn".swapcase
+ #overwite ! but does same thing
+ p "iNvEsTiGaTiOn".swapcase!
+		# => “InVeStIgAtIoN”
 
-puts "enhance".<??>
-# => "    enhance    "
 
-puts "Stop! You’re under arrest!".<??>
-# => "STOP! YOU’RE UNDER ARREST!"
+puts("----------------------")
 
-puts "the usual".<??>
-#=> "the usual suspects"
+ p "zom".insert(1,'o')
+		# => “zoom”
 
-puts " suspects".<??>
-# => "the usual suspects"
+puts("---------------------------")
 
-puts "The case of the disappearing last letter".<??>
-puts "The case of the disappearing last letter".<??>
-# => "The case of the disappearing last lette"
+  p "enhance".center(20)
+		# => "    enhance    "
 
-puts "The mystery of the missing first letter".<??>
-# => "he mystery of the missing first letter"
+puts("---------------------------")
 
-puts "Elementary,    my   dear        Watson!".<??>
-# => "Elementary, my dear Watson!"
+ p "Stop! You’re under arrest!".upcase
+		# => "STOP! YOU’RE UNDER ARREST!"
 
-puts "z".<??>
-# => 122 
+puts("---------------------------")
+
+ p "the usual".insert(9,' suspects')
+		#=> "the usual suspects"
+ a = "the usual" 
+ a << " suspects"
+ p a.concat('')
+
+puts("------------------------------")
+
+ p " suspects".prepend('the usual')
+		# => "the usual suspects"
+
+puts("----------------------------------------")
+
+ p "The case of the disappearing last letter".chomp("r")
+ p "The case of the disappearing last letter".chop
+		# => "The case of the disappearing last lette"
+
+puts("----------------------------------------")
+
+ p "The mystery of the missing first letter".delete("T")
+		# => "he mystery of the missing first letter"
+
+puts("----------------------------------------")
+
+ p "Elementary,    my   dear        Watson!".squeeze
+
+		# => "Elementary, my dear Watson!"
+
+puts("----------------------------")
+ p "z".bytes
+ #or
+ p "z".ord #turns character to a number
+		# => 122 
 # (What is the significance of the number 122 in relation to the character z?)
-# 122 in relation with the character z, is that 122 is the number of bytes within the string z.
+#the amount of bytes 122 has
 
-puts "How many times does the letter 'a' appear in this string?".<??>
-# => 4
+puts("----------------------")
+
+ p "How many times does the letter 'a' appear in this string?".count("a")
+ p "How many times does the letter 'a' appear in this string?".scan(/a/).length
+
+		# => 4
