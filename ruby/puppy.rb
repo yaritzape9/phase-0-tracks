22cd03bb91d8f6d65f9709201ed9f227dd2a0803
yaritza perez <yaritzape9@gmail.com>
2016-10-18 17:56:47 -0700

    added pseudocoe to finish puppy class

---
+++ ruby/puppy.rb
@@ -0,0 +1,23 @@
+=begin
+Class Puppy
+
+	Have characteristics and behavior
+
+		Characteristics:
+			name: babz
+			Age: 3
+			adoptable: yes
+			cuddly: yes 
+			kid_freindly: yes
+			fur_color: grey
+
+
+		Behavior:
+			jump: a lot
+			fit: yes
+			adventurous: yes
+			barks: yes
+			chews: no
+			digs: no
+
+=end
