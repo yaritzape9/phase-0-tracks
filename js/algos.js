9f3ca5399898d44224795698e0a29a855e8d05b6
yaritza perez <yaritzape9@gmail.com>
2016-10-31 02:12:17 -0700

    finished second algo

--- js/algos.js
+++ js/algos.js
@@ -24,3 +24,40 @@ function longestString(str) {
  var words = ["hellos","longstring","my name is Yaritza"];
 longestString(words);
 console.log(longestString(["bob", "Ysvettelana", "Genesis", "Lovely", "Cindy"]))
+/*
+it takes two objects and checks if objects 
+share at least one key-value pair
+
+it will select first key-value pair of object one
+and iterate through key-value pairs of object two
+
+if there is a match it will return true
+else it will repeat until no more key-value pairs to match
+*/
+console.log("release1----------------------------------")
+function compare(first_obj, second_obj){
+	
+	var match = 0;
+	var keys_arr = Object.keys(second_obj);
+
+	for (var i = 0; i < keys_arr.length; i++){
+		var key_to_obj = keys_arr[i];
+		if (first_obj[key_to_obj] === second_obj[key_to_obj]){
+			match++;
+			console.log("true");
+		}
+	}
+	console.log("you have " + match + " match(s)")
+}
+
+// driver code for compare
+var person1 = {name: "Kurtis", age: 27};
+var person2 = {name: "Francesc", age: 28};
+compare(person1, person2);
+console.log("********")
+var person3 = {name: "Joe", age: 20, haircolor:"brown"};
+var person4 = {name: "Lana", age: 28, haircolor:"brown"};
+compare(person3, person4);
+console.log("********")
+compare(person1,person4)
+
