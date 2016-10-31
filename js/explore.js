25db63a06fce9c50354e6c312d7174d30c362f53
yaritza perez <yaritzape9@gmail.com>
2016-10-30 20:32:28 -0700

    added psuedo code and function

---
+++ js/explore.js
@@ -0,0 +1,15 @@
+/*psuedocode 
+	-I have to start off with a variable that will hold my new string. So it will be an empty string.
+	-I will loop through the string.
+	-I will then make my new variable equal to my string index of i plus my new string so as it adds together it will reverse the string
+	-Then the loop will break when its done going through the whole string.
+	-From there I will return the value of my new variable which I will name newString.
+
+	-Then I will call my method and put in a string. 
+	- From there I ill make an if and else statement that will print out the method answer or Not equal. I also stored my method in a variable to make it easier.
+*/
+
+function reverseString(string) {
+    
+ 
+}
