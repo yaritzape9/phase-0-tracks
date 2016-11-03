dbf74d42f3bc715f38461dcfd1b1e6cc6af94878
yaritza perez <yaritzape9@gmail.com>
2016-11-03 04:01:38 -0700

    code for derby

--- databases/create_derby.sql
+++ databases/create_derby.sql
@@ -5,25 +5,26 @@
 --   team VARCHAR(255)
 -- );
 
--- INSERT INTO players (name,player_number, team) VALUES ("Grave Danger", 6, "Team Unicorn");
--- INSERT INTO players (name,player_number, team) VALUES ("Beyonslay", 7, "Wrecking Belles");
+-- INSERT INTO players (name, player_number, team) VALUES ("Paula Bunion", 45, "Wrecking Belles");
+-- INSERT INTO players (name, player_number, team) VALUES ("Grave Danger", 6, "Team Unicorn");
+-- INSERT INTO players (name, player_number, team) VALUES ("April Bloodgate", 12, "Team Unicorn");
 
---  CREATE TABLE teams (
---   id INTEGER PRIMARY KEY,
---   name VARCHAR(255)
--- );
+-- CREATE TABLE teams ( 
+-- 	id INTEGER PRIMARY KEY,
+--  team_name  VARCHAR(255)
+-- ); 
 
--- INSERT INTO teams (name) VALUES ("Team Unicorn");
--- INSERT INTO teams (name) VALUES ("Wrecking Belles");
+-- INSERT INTO teams (team_name) VALUES ("Wrecking Belles");
+-- INSERT INTO teams (team_name) VALUES ("Team Unicorn");
 
- CREATE TABLE players (
-  id INTEGER PRIMARY KEY,
-  name VARCHAR(255),
-  player_number INT,
-  team_id INT,
-  FOREIGN KEY (team_id) REFERENCES teams(id)
-);
+-- CREATE TABLE players ( 
+-- 	id INTEGER PRIMARY KEY,
+-- 	name VARCHAR(255), 
+-- 	player_num INT,
+-- 	team_id INT,
+-- 	FOREIGN KEY (team_id) REFERENCES teams(id)
+-- ); 
 
-INSERT INTO players (name,player_number, team_id) VALUES ("Grave Danger", 6, 1);
-INSERT INTO players (name,player_number, team_id) VALUES ("Beyonslay", 7, 2);
-INSERT INTO players (name,player_number, team_id) VALUES ("Terror Swift", 48, 2);
+INSERT INTO players (name, player_num, team_id) VALUES ("Terror Swift", 48 , 1);
+INSERT INTO players (name, player_num, team_id) VALUES ("Frisky Disco", 100, 1);
+INSERT INTO players (name, player_num, team_id) VALUES ("Bloodbath and Beyond", 3, 2);
