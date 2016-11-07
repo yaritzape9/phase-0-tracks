ba4dfa0236c5b4c30b9e970a84f077db3fb7da50
yaritza perez <yaritzape9@gmail.com>
2016-11-07 02:36:55 -0800

    added a method

--- databases/8.5project/dictionary.rb
+++ databases/8.5project/dictionary.rb
@@ -1,6 +1,38 @@
-require 'rubygems'
+
 require 'sqlite3'
 
-DBNAME = "Dictionary_info.sqlite"
+DBNAME = "Dictionary_info.db"
 
-DB = SQLite3::Database.new( DBNAME )
+db = SQLite3::Database.new(DBNAME)
+
+create_table_cmd = <<-SQL
+	CREATE TABLE dictionary_phrases ( 
+	id INTEGER PRIMARY KEY,
+	spanish_phrase VARCHAR(255),
+	english_phrase VARCHAR(255)
+  )
+SQL
+
+db.execute(create_table_cmd)
+
+def store_phrases(db,spanish_phrase,english_phrase)
+	db.execute("INSERT INTO dictionary_phrases (spanish_phrase,english_phrase) VALUES (?, ?, ?, ?)", [spanish_phrase,english_phrase])	
+end
+
+def edit_phrase
+
+end
+
+def delete_phrase
+
+end
+
+def view_all_phrases
+
+end
+
+##UI
+
+
+
+
