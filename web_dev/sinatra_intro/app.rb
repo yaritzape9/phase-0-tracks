5801f97548213c4fb75538d2de388bbb4cc04bfa
yaritza perez <yaritzape9@gmail.com>
2016-11-13 22:21:17 -0800

    added commands

--- web_dev/sinatra_intro/app.rb
+++ web_dev/sinatra_intro/app.rb
@@ -7,18 +7,20 @@ db.results_as_hash = true
 
 # write a basic GET route
 # add a query parameter
-# GET /
+# GET /(home directory '/')
+#localhost.8888/?name=Jen&age=35
 get '/' do
   "#{params[:name]} is #{params[:age]} years old."
 end
 
 # write a GET route with
 # route parameters
+#localhost.888/?about/Jen
 get '/about/:person' do
   person = params[:person]
   "#{person} is a programmer, and #{person} is learning Sinatra."
 end
-
+#localhost.888/?lynda/loves/robert
 get '/:person_1/loves/:person_2' do
   "#{params[:person_1]} loves #{params[:person_2]}"
 end
@@ -44,3 +46,40 @@ get '/students/:id' do
   student = db.execute("SELECT * FROM students WHERE id=?", [params[:id]])[0]
   student.to_s
 end
+
+
+get '/contact/:address' do
+  address = params[:address]
+  "This is an email adress: #{address} and they love to visit your website!!"
+end
+
+#output : http://localhost:9393/contact/plants@gmail.com
+#This is an email adress: plants@gmail.com and they love to visit your website!!
+
+get '/great_job' do
+  name = params[:name]
+  if name 
+    "Good job #{name}!"
+  else
+    "Good job!"
+  end
+end
+# output : http://localhost:9393/great_job?name=yaritza
+# Good job yaritza!
+#   http://localhost:9393/great_job
+# Good job!
+
+get '/:num_1/:num_2' do 
+
+  num_1 = (params[:num_1]).to_i
+  num_2 = (params[:num_2]).to_i
+  answer = num_1 + num_2
+  answer.to_s
+end
+
+#output http://localhost:9393/1/2
+#3
+
+
+
+
